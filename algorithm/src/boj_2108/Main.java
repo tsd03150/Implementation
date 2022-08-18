35f102efc27af0f9e4da6e7ed44d96b29f9903b8
tsd03150 <tsd03150@naver.com>
2022-08-18 17:18:10 +0900

    220818_commit

--- algorithm/src/boj_2108/Main.java
+++ algorithm/src/boj_2108/Main.java
@@ -1,9 +1,6 @@
 package boj_2108;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 class Number implements Comparable<Number>{
     int num;
@@ -29,52 +26,68 @@ class Main {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
-        int[] arr = new int[n];
+        ArrayList<Integer> arr = new ArrayList<>();
+        ArrayList<Number> tmp = new ArrayList<>();
         long sum = 0;
         for (int i = 0; i < n; i++) {
-            arr[i] = kb.nextInt();
-            sum += arr[i];
-        }
-        Arrays.sort(arr);
-        System.out.println(Math.round(sum / (double)n));
-        System.out.println(arr[n / 2]);
-
-        ArrayList<Number> tmp = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            int num = arr[i];
-            boolean flag = false;
-            for (int j = 0; j < tmp.size(); j++) {
-                if (tmp.get(j).num == num) {
-                    tmp.get(j).cnt++;
-                    flag = true;
+            int num = kb.nextInt();
+            sum += num;
+            if (arr.contains(num)) {
+                boolean flag = false;
+                for (Number number : tmp) {
+                    if (number.num == num) {
+                        flag = true;
+                        number.cnt++;
+                    }
+                }
+                if (!flag) {
+                    tmp.add(new Number(num, 1));
                 }
             }
-            if (!flag) {
-                tmp.add(new Number(num, 0));
+            arr.add(num);
+        }
+        Collections.sort(arr);
+        // 평균값
+        System.out.println(Math.round(sum / (double) n));
+        // 중앙값
+        System.out.println(arr.get(n / 2));
+        // 최빈값
+        if (tmp.size() == 0) {
+            if (arr.size() == 1) {
+                System.out.println(arr.get(0));
+            } else {
+                System.out.println(arr.get(1));
             }
-        }
-        Collections.sort(tmp);
-        int min = tmp.get(0).num;
-        int cnt = tmp.get(0).cnt;
-        boolean flag = false;
-        for (int i = 1; i < tmp.size(); i++) {
-            if (cnt == tmp.get(i).cnt && min < tmp.get(i).num) {
-                System.out.println(tmp.get(i).num);
-                flag = true;
-                break;
-            }
-        }
-        if (!flag) {
-            System.out.println(min);
-        }
-        int f = arr[0];
-        int e = arr[n - 1];
-        if (f == e) {
-            System.out.println("0");
-        } else if (f < 0 && e > 0) {
-            System.out.println(Math.abs(f) + Math.abs(e));
+        } else if (tmp.size() == 1) {
+            System.out.println(tmp.get(0).num);
         } else {
-            System.out.println(Math.abs(f - e));
+            Collections.sort(tmp);
+            int num = tmp.get(0).num;
+            int max = tmp.get(0).cnt;
+            boolean flag = false;
+            int idx = 0;
+            for (int i = 1; i < tmp.size(); i++) {
+                if (max == tmp.get(i).cnt && num < tmp.get(i).num) {
+                    flag = true;
+                    idx = i;
+                    break;
+                }
+            }
+            if (flag) {
+                System.out.println(tmp.get(idx).num);
+            } else {
+                System.out.println(num);
+            }
+        }
+        // 범위 값
+        int a = arr.get(0);
+        int b = arr.get(n - 1);
+        if (a < 0 && b < 0) {
+            System.out.println(Math.abs(a) - Math.abs(b));
+        } else if (a >= 0 && b > 0) {
+            System.out.println(Math.abs(a - b));
+        } else {
+            System.out.println(Math.abs(a) + Math.abs(b));
         }
     }
 }
